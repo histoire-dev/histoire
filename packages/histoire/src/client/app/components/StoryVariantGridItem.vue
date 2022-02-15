3a96827ed1895deafa32e4fa7efb2393d821b8d1
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-02-16 00:17:37 +0100

    fix: grid variant item text not selectable

--- packages/histoire/src/client/app/components/StoryVariantGridItem.vue
+++ packages/histoire/src/client/app/components/StoryVariantGridItem.vue
@@ -21,27 +21,24 @@ const { isActive, targetRoute } = useCurrentVariantRoute(variant)
 </script>
 
 <template>
-  <RouterLink
-    :to="targetRoute"
-    class="htw-cursor-default htw-flex htw-flex-col htw-space-y-1"
-  >
+  <div class="htw-cursor-default htw-flex htw-flex-col htw-gap-y-1">
     <!-- Header -->
-    <div>
-      <div
+    <div class="htw-flex-none htw-flex htw-items-center">
+      <RouterLink
         :to="targetRoute"
-        class="htw-rounded htw-w-max htw-px-1.5 htw-cursor-pointer"
+        class="htw-rounded htw-w-max htw-px-2 htw-py-0.5 htw-min-w-16 htw-cursor-pointer"
         :class="{
           'hover:htw-bg-zinc-200 htw-text-zinc-500 dark:hover:htw-bg-zinc-800': !isActive,
           'htw-bg-primary-200 hover:htw-bg-primary-300 htw-text-primary-800 dark:htw-bg-primary-700 dark:hover:htw-bg-primary-800 dark:htw-text-primary-200': isActive,
         }"
       >
         {{ variant.title }}
-      </div>
+      </RouterLink>
     </div>
 
     <!-- Body -->
     <div
-      class="htw-border htw-bg-white dark:htw-bg-zinc-700 htw-rounded-lg htw-h-full htw-p-4"
+      class="htw-border htw-bg-white dark:htw-bg-zinc-700 htw-rounded-lg htw-flex-1 htw-p-4"
       :class="{
         'htw-border-zinc-100 dark:htw-border-zinc-800': !isActive,
         'htw-border-primary-200 dark:htw-border-primary-900': isActive,
@@ -53,5 +50,5 @@ const { isActive, targetRoute } = useCurrentVariantRoute(variant)
         :story="story"
       />
     </div>
-  </RouterLink>
+  </div>
 </template>
