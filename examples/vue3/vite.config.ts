8a088e24de39def843e3ed526f399e90a060455e
Hugo ATTAL <hugoattal@hotmail.fr>
2022-02-16 01:31:36 +0100

    feat: Add vite config (#18) (#23)

---
+++ examples/vue3/vite.config.ts
@@ -0,0 +1,9 @@
+/// <reference types="histoire" />
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  histoire: {
+    // Alternative way of specifying histoire config
+  },
+})
