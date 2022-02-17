f31239393e4efc27ec0d824c78abbb254a2cd724
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-02-17 16:10:52 +0100

    feat: hatched pattern in responsive view

--- packages/histoire/src/client/app/components/StoryVariantSingleView.vue
+++ packages/histoire/src/client/app/components/StoryVariantSingleView.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { useEventListener } from '@vueuse/core'
 import { PropType, ref, watch, toRaw, computed } from 'vue'
+import { Icon } from '@iconify/vue'
 import { Story, Variant } from '../types'
 import { STATE_SYNC } from '../util/const.js'
 import BaseSplitPane from './base/BaseSplitPane.vue'
+import HatchedPattern from './misc/HatchedPattern.vue'
 
 const props = defineProps({
   variant: {
@@ -61,14 +63,28 @@ function onIframeLoad () {
 </script>
 
 <template>
-  <div class="htw-border htw-border-gray-100 dark:htw-border-gray-800 htw-rounded-lg htw-h-full">
+  <div class="htw-h-full htw-flex htw-flex-col htw-gap-2">
+    <div class="htw-flex-none">
+      <!-- Variant title -->
+      <div class="htw-flex htw-items-center htw-gap-1 htw-text-gray-500">
+        <Icon
+          :icon="variant.icon ?? 'carbon:cube'"
+          class="base-list-item-link-icon htw-w-4 htw-h-4 htw-opacity-50"
+          :class="[
+            variant.iconColor ? 'bind-icon-color' : 'htw-text-gray-500',
+          ]"
+        />
+        <span>{{ variant.title }}</span>
+      </div>
+    </div>
+
     <BaseSplitPane
       save-id="story-single-responsive"
       :min="10"
       :max="6000"
       :default-split="800"
       fixed
-      class="htw-w-full"
+      class="htw-w-full htw-h-full htw-flex-1 htw-border htw-border-gray-100 dark:htw-border-gray-800 htw-rounded-lg"
     >
       <template #first>
         <div class="htw-p-4 htw-h-full htw-overflow-hidden htw-bg-white dark:htw-bg-gray-700 htw-rounded-l-lg htw-relative">
@@ -93,7 +109,13 @@ function onIframeLoad () {
         </div>
       </template>
       <template #last>
-        <div class="htw-w-full htw-h-full htw-bg-gray-200 dark:htw-bg-gray-850 htw-rounded-r-lg htw-border-l-2 htw-border-gray-500/10 dark:htw-border-gray-700/30" />
+        <div
+          class="htw-w-full htw-h-full htw-bg-gray-200 dark:htw-bg-gray-850 htw-rounded-r-lg htw-border-l-2 htw-border-gray-500/10 dark:htw-border-gray-700/30"
+        >
+          <HatchedPattern
+            class="htw-w-full htw-h-full htw-text-black/[2%] dark:htw-text-white/[2%]"
+          />
+        </div>
       </template>
     </BaseSplitPane>
   </div>
---
+++ packages/histoire/src/client/app/components/misc/HatchedPattern.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg fill="none">
+    <pattern
+      id="hatch"
+      width="32"
+      height="32"
+      patternUnits="userSpaceOnUse"
+    >
+      <path
+        d="M32,0 L0,32
+           M-1,1 l2,-2
+           M31,33 l2,-2"
+        stroke-width="2"
+        stroke="currentColor"
+      />
+    </pattern>
+    <rect
+      x="0"
+      y="0"
+      width="100%"
+      height="100%"
+      fill="url(#hatch)"
+    />
+  </svg>
+</template>
