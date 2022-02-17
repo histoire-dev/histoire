3a02e6a692915ffa6c20552b8d518a9b8ab29af0
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-02-17 23:09:59 +0100

    feat: improved keyboard tooltips

--- packages/histoire/src/client/app/util/tooltip.ts
+++ packages/histoire/src/client/app/util/tooltip.ts
@@ -3,11 +3,11 @@ import { formatKey } from './keyboard.js'
 
 export function makeTooltip (descriptionHtml: string, keyboardShortcut: ({ isMac: boolean }) => string) {
   return {
-    content: `${descriptionHtml}<br><div class="htw-flex htw-items-center htw-gap-1 htw-mt-1">${genKeyboardShortcutHtml(keyboardShortcut({ isMac }))}</div>`,
+    content: `<div>${descriptionHtml}</div><div class="htw-flex htw-items-center htw-gap-1 htw-mt-2 htw-text-sm">${genKeyboardShortcutHtml(keyboardShortcut({ isMac }))}</div>`,
     html: true,
   }
 }
 
 function genKeyboardShortcutHtml (shortcut: string) {
-  return shortcut.split('+').map(k => k.trim()).map(key => `<span class="htw-bg-gray-700 htw-px-1 htw-rounded-sm">${formatKey(key)}</span>`).join('')
+  return shortcut.split('+').map(k => k.trim()).map(key => `<span class="htw-bg-gray-500 htw-border-b-2 htw-border-gray-600 htw-px-1 htw-rounded-sm">${formatKey(key)}</span>`).join('')
 }
