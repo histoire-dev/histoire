ae7d47cea3bf566bf3439f9fd2ea175166c9fb7f
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-02-23 00:26:53 +0100

    feat: restart on config change, closes #28

--- packages/histoire/src/node/commands/dev.ts
+++ packages/histoire/src/node/commands/dev.ts
@@ -1,14 +1,58 @@
+import { resolveConfigFile } from '../config.js'
 import { createContext } from '../context.js'
 import { createServer } from '../server.js'
+import chokidar from 'chokidar'
+import pc from 'picocolors'
 
 export interface DevOptions {
   port: number
 }
 
 export async function devCommand (options: DevOptions) {
-  const ctx = await createContext({
-    mode: 'dev',
-  })
-  const { server } = await createServer(ctx, options.port)
-  server.printUrls()
+  let stop: () => Promise<void>
+
+  async function start () {
+    const ctx = await createContext({
+      mode: 'dev',
+    })
+    const { server, close } = await createServer(ctx, options.port)
+    server.printUrls()
+
+    // Histoire config watcher
+    let watcher: chokidar.FSWatcher
+    if (server.config.configFile) {
+      watcher = chokidar.watch(server.config.configFile, {
+        ignoreInitial: true,
+      })
+      watcher.on('change', () => {
+        restart('Vite')
+      })
+    }
+
+    return async () => {
+      await watcher?.close()
+      await close()
+    }
+  }
+
+  async function restart (source: string) {
+    if (stop) {
+      console.log(pc.blue(`${source} config changed, restarting...`))
+      stop()
+      stop = null // Don't call stop again until new start() is done
+      stop = await start()
+    }
+  }
+
+  stop = await start()
+
+  const configFile = await resolveConfigFile()
+  if (configFile) {
+    const watcher = chokidar.watch(configFile, {
+      ignoreInitial: true,
+    })
+    watcher.on('change', () => {
+      restart('Histoire')
+    })
+  }
 }
