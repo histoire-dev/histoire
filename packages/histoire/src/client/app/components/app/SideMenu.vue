4b9f6644266ab71ddd1bef327995765b905ec09f
Hugo <hugoattal@hotmail.fr>
2022-03-16 23:28:21 +0100

    Add cool transition

--- packages/histoire/src/client/app/components/app/SideMenu.vue
+++ packages/histoire/src/client/app/components/app/SideMenu.vue
@@ -10,21 +10,23 @@ const emits = defineEmits<{(e: 'close'): void}>()
 </script>
 
 <template>
-  <div
-    v-if="isOpened"
-    class="htw-absolute htw-z-10 htw-bg-white dark:htw-bg-gray-700 htw-w-screen htw-h-screen htw-inset-0 htw-overflow-y-hidden htw-flex htw-flex-col"
-  >
-    <div class="htw-p-4 htw-h-16 htw-flex htw-border-b htw-border-gray-100 dark:htw-border-gray-800 htw-items-center htw-justify-end">
-      <a
-        class="htw-p-1 hover:htw-text-primary-500 dark:hover:htw-text-primary-400 htw-cursor-pointer"
-        @click="emits('close')"
-      >
-        <Icon
-          icon="carbon:close"
-          class="htw-w-8 htw-h-8 htw-shrink-0"
-        />
-      </a>
+  <transition name="__histoire-fade">
+    <div
+      v-if="isOpened"
+      class="htw-absolute htw-z-10 htw-bg-white dark:htw-bg-gray-700 htw-w-screen htw-h-screen htw-inset-0 htw-overflow-y-hidden htw-flex htw-flex-col"
+    >
+      <div class="htw-p-4 htw-h-16 htw-flex htw-border-b htw-border-gray-100 dark:htw-border-gray-800 htw-items-center htw-justify-end">
+        <a
+          class="htw-p-1 hover:htw-text-primary-500 dark:hover:htw-text-primary-400 htw-cursor-pointer"
+          @click="emits('close')"
+        >
+          <Icon
+            icon="carbon:close"
+            class="htw-w-8 htw-h-8 htw-shrink-0"
+          />
+        </a>
+      </div>
+      <slot />
     </div>
-    <slot />
-  </div>
+  </transition>
 </template>
