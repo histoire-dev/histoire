9af124a760769dd7f19267e4d8dbbcfb3d4d7e81
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-03-17 12:00:44 +0100

    fix(mobile): bigger hit targets in select lists

--- packages/histoire/src/client/app/components/story/StoryListItem.vue
+++ packages/histoire/src/client/app/components/story/StoryListItem.vue
@@ -33,7 +33,7 @@ useScrollOnActive(active, el)
           storyId: story.id,
         },
       }"
-      class="htw-pl-0.5 htw-pr-2 htw-py-1.5 htw-m-1 htw-rounded-sm"
+      class="htw-pl-0.5 htw-pr-2 htw-py-2 md:htw-py-1.5 htw-m-1 htw-rounded-sm"
     >
       <span class="bind-tree-margin htw-flex htw-items-center htw-gap-2 htw-pl-5 htw-min-w-0">
         <Icon
