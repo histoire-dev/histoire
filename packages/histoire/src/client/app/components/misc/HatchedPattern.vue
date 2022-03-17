3bd76dc0b3becf24517629b07975835d4b05b2b9
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-03-17 11:36:14 +0100

    fix: css class

--- packages/histoire/src/client/app/components/misc/HatchedPattern.vue
+++ packages/histoire/src/client/app/components/misc/HatchedPattern.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="hatched-patten" />
+  <div class="__histoire-hatched-pattern" />
 </template>
 
-<style>
-.hatched-patten {
-    background: repeating-linear-gradient(135deg,
-    transparent 0px,
-    transparent 32px,
-    currentColor 32px,
-    currentColor 64px);
+<style scoped>
+.__histoire-hatched-pattern {
+  background: repeating-linear-gradient(135deg,
+  transparent 0px,
+  transparent 32px,
+  currentColor 32px,
+  currentColor 64px);
 }
 </style>
