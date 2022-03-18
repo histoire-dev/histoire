5889945ebea6a5b25b6c2bd9f93266cce2f3f0bd
Hugo <hugoattal@hotmail.fr>
2022-03-18 23:45:33 +0100

    Add textarea

--- packages/histoire-controls/src/index.ts
+++ packages/histoire-controls/src/index.ts
@@ -2,6 +2,7 @@ import type { App } from 'vue'
 import HstCheckboxVue from './components/checkbox/HstCheckbox.vue'
 import HstTextVue from './components/text/HstText.vue'
 import HstNumberVue from './components/number/HstNumber.vue'
+import HstTextareaVue from './components/textarea/HstTextarea.vue'
 
 export const HstCheckbox = HstCheckboxVue
 export const HstText = HstTextVue
@@ -10,4 +11,5 @@ export function registerVueComponents (app: App) {
   app.component('HstCheckbox', HstCheckboxVue)
   app.component('HstText', HstTextVue)
   app.component('HstNumber', HstNumberVue)
+  app.component('HstTextarea', HstTextareaVue)
 }
