fc3f3fdd053b75f152979e1af4621ea5871163d1
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-03-20 14:00:58 +0100

    chore: fix controls stories

--- packages/histoire-controls/src/components/checkbox/HstCheckbox.vue
+++ packages/histoire-controls/src/components/checkbox/HstCheckbox.vue
@@ -47,7 +47,7 @@ watch(path, value => {
   >
     <div class="htw-text-white htw-w-[16px] htw-h-[16px] htw-relative">
       <div
-        class="htw-border group-active:htw-bg-gray-500/20 htw-rounded-sm htw-box-border htw-absolute htw-inset-0 htw-transition-border htw-duration-150 htw-ease-out"
+        class="htw-border htw-border-solid group-active:htw-bg-gray-500/20 htw-rounded-sm htw-box-border htw-absolute htw-inset-0 htw-transition-border htw-duration-150 htw-ease-out"
         :class="[
           modelValue
             ? 'htw-border-primary-500 htw-border-8'
