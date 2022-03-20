e4b032159320fde4d1ffcbeda809c0b7c049e5cb
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-03-20 20:05:01 +0100

    test(ci): try fix?

--- examples/vue3/cypress/integration/story-preview.js
+++ examples/vue3/cypress/integration/story-preview.js
@@ -5,7 +5,7 @@ describe('Story preview', () => {
     .its('0.contentDocument.body').should('not.be.empty')
     .then(cy.wrap)
 
-  it('should display the story variants', () => {
+  it('should display the untitled variant', () => {
     cy.visit('/')
     cy.get('[data-test-id="story-list-item"]').contains('Demo').click()
     cy.get('[data-test-id="story-variant-list-item"]').should('have.length', 2)
@@ -13,6 +13,12 @@ describe('Story preview', () => {
     cy.get('[data-test-id="story-variant-single-view"]').contains('untitled')
     getIframeBody().contains('Hello world!')
     cy.get('[data-test-id="story-source-code"]').contains('<Demo message="Hello world!" />')
+  })
+
+  it('should display the second variant', () => {
+    cy.visit('/')
+    cy.get('[data-test-id="story-list-item"]').contains('Demo').click()
+    cy.get('[data-test-id="story-variant-list-item"]').should('have.length', 2)
     cy.get('[data-test-id="story-variant-list-item"]').contains('Variant 2').click()
     cy.get('[data-test-id="story-variant-single-view"]').contains('Variant 2')
     getIframeBody().contains('Meow!')
