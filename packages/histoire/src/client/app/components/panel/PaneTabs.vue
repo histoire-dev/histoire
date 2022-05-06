d42f438c11711146405fc0a3a4ff8be7f2f7bdd6
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-05-06 13:44:50 +0200

    feat: tabs overflow menu, fix #73

--- packages/histoire/src/client/app/components/panel/PaneTabs.vue
+++ packages/histoire/src/client/app/components/panel/PaneTabs.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
 import BaseTab from '../base/BaseTab.vue'
 import { useEventsStore } from '../../stores/events'
+import BaseOverflowMenu from '../base/BaseOverflowMenu.vue'
+import BaseOverflowTab from '../base/BaseOverflowTab.vue'
 
 const eventsStore = useEventsStore()
 </script>
 
 <template>
-  <nav class="htw-h-10 htw-flex-none htw-border-b htw-border-gray-100 dark:htw-border-gray-750">
+  <BaseOverflowMenu class="htw-h-10 htw-flex-none htw-border-b htw-border-gray-100 dark:htw-border-gray-750">
     <BaseTab
       :to="{ ...$route, query: { ...$route.query, tab: '' } }"
       :matched="!$route.query.tab"
@@ -31,5 +33,32 @@ const eventsStore = useEventsStore()
         {{ eventsStore.unseen <=99 ? eventsStore.unseen : "99+" }}
       </span>
     </BaseTab>
-  </nav>
+
+    <template #overflow>
+      <BaseOverflowTab
+        :to="{ ...$route, query: { ...$route.query, tab: '' } }"
+        :matched="!$route.query.tab"
+      >
+        Controls
+      </BaseOverflowTab>
+      <BaseOverflowTab
+        :to="{ ...$route, query: { ...$route.query, tab: 'docs' } }"
+        :matched="$route.query.tab === 'docs'"
+      >
+        Docs
+      </BaseOverflowTab>
+      <BaseOverflowTab
+        :to="{ ...$route, query: { ...$route.query, tab: 'events' } }"
+        :matched="$route.query.tab === 'events'"
+      >
+        Events
+        <span
+          v-if="eventsStore.unseen"
+          class="htw-text-center htw-text-xs htw-mx-1 htw-px-0.5 htw-h-4 htw-min-w-4 htw-rounded-full active htw-bg-primary-500 htw-text-white dark:htw-text-black"
+        >
+          {{ eventsStore.unseen <=99 ? eventsStore.unseen : "99+" }}
+        </span>
+      </BaseOverflowTab>
+    </template>
+  </BaseOverflowMenu>
 </template>
