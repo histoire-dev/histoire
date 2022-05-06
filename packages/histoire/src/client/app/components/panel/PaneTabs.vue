7a14c4dd86e9516b6ff57438b4f5aad1d8e7d536
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-05-06 12:00:09 +0200

    refactor: extract to PaneTabs.vue

---
+++ packages/histoire/src/client/app/components/panel/PaneTabs.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import BaseTab from '../base/BaseTab.vue'
+import { useEventsStore } from '../../stores/events'
+
+const eventsStore = useEventsStore()
+</script>
+
+<template>
+  <nav class="htw-h-10 htw-flex-none htw-border-b htw-border-gray-100 dark:htw-border-gray-750">
+    <BaseTab
+      :to="{ ...$route, query: { ...$route.query, tab: '' } }"
+      :matched="!$route.query.tab"
+    >
+      Controls
+    </BaseTab>
+    <BaseTab
+      :to="{ ...$route, query: { ...$route.query, tab: 'docs' } }"
+      :matched="$route.query.tab === 'docs'"
+    >
+      Docs
+    </BaseTab>
+    <BaseTab
+      :to="{ ...$route, query: { ...$route.query, tab: 'events' } }"
+      :matched="$route.query.tab === 'events'"
+    >
+      Events
+      <span
+        v-if="eventsStore.unseen"
+        class="htw-text-center htw-text-xs htw-mx-1 htw-px-0.5 htw-h-4 htw-min-w-4 htw-rounded-full active htw-bg-primary-500 htw-text-white dark:htw-text-black"
+      >
+        {{ eventsStore.unseen <=99 ? eventsStore.unseen : "99+" }}
+      </span>
+    </BaseTab>
+  </nav>
+</template>
--- packages/histoire/src/client/app/components/panel/StorySidePanel.vue
+++ packages/histoire/src/client/app/components/panel/StorySidePanel.vue
@@ -1,21 +1,18 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import { useStoryStore } from '../../stores/story'
-import { useEventsStore } from '../../stores/events'
 
 import BaseSplitPane from '../base/BaseSplitPane.vue'
 import BaseEmpty from '../base/BaseEmpty.vue'
-import BaseTab from '../base/BaseTab.vue'
 import StoryControls from './StoryControls.vue'
 import StoryDocs from './StoryDocs.vue'
 import StoryEvents from './StoryEvents.vue'
 import StorySourceCode from './StorySourceCode.vue'
+import PaneTabs from './PaneTabs.vue'
 
 const storyStore = useStoryStore()
-const eventsStore = useEventsStore()
 
-const router = useRouter()
 const route = useRoute()
 
 const panelContentComponent = computed(() => {
@@ -48,32 +45,7 @@ const panelContentComponent = computed(() => {
   >
     <template #first>
       <div class="htw-flex htw-flex-col htw-h-full">
-        <nav class="htw-h-10 htw-flex-none htw-border-b htw-border-gray-100 dark:htw-border-gray-750">
-          <BaseTab
-            :to="{ ...$route, query: { ...$route.query, tab: '' } }"
-            :matched="!$route.query.tab"
-          >
-            Controls
-          </BaseTab>
-          <BaseTab
-            :to="{ ...$route, query: { ...$route.query, tab: 'docs' } }"
-            :matched="$route.query.tab === 'docs'"
-          >
-            Docs
-          </BaseTab>
-          <BaseTab
-            :to="{ ...$route, query: { ...$route.query, tab: 'events' } }"
-            :matched="$route.query.tab === 'events'"
-          >
-            Events
-            <span
-              v-if="eventsStore.unseen"
-              class="htw-text-center htw-text-gray-900 dark:htw-text-gray-100 htw-text-xs htw-mx-1 htw-px-0.5 htw-h-4 htw-min-w-4 htw-rounded-full active htw-bg-primary-500 htw-text-white dark:htw-text-black"
-            >
-              {{ eventsStore.unseen <=99 ? eventsStore.unseen : "99+" }}
-            </span>
-          </BaseTab>
-        </nav>
+        <PaneTabs />
 
         <component
           :is="panelContentComponent"
