4d31997c8e16dba7059ee2079f85d9399f465991
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-05-09 16:30:57 +0200

    fix: mark all vue packages as external

--- packages/histoire/vite.config.ts
+++ packages/histoire/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     rollupOptions: {
       external: [
         /\$histoire/,
+        /@vue\/*/,
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         ...Object.keys(require('./package.json').dependencies),
       ],
