327c7cfb66062fe9614d7addb56ae44364014ddd
Saman Hosseini <bounoable@gmail.com>
2022-06-23 15:27:16 +0200

    fix: stringify functions in `toRawDeep`

--- packages/histoire/src/client/app/util/state.ts
+++ packages/histoire/src/client/app/util/state.ts
@@ -2,10 +2,15 @@ import { unref, isRef } from 'vue'
 
 const isObject = (val) => val !== null && typeof val === 'object'
 
-export function toRawDeep (val, seen = new Set()) {
+export function toRawDeep(val, seen = new Set()) {
   const unwrappedValue = isRef(val) ? unref(val) : val
+  const valueType = typeof unwrappedValue
 
-  if (typeof unwrappedValue === 'symbol') {
+  if (valueType === 'symbol') {
+    return unwrappedValue.toString()
+  }
+
+  if (valueType === 'function') {
     return unwrappedValue.toString()
   }
 
@@ -20,13 +25,16 @@ export function toRawDeep (val, seen = new Set()) {
   seen.add(unwrappedValue)
 
   if (Array.isArray(unwrappedValue)) {
-    return unwrappedValue.map(value => toRawDeep(value, seen))
+    return unwrappedValue
+      .map((value) => toRawDeep(value, seen))
+      .filter((val) => val !== void 0)
   }
 
   return toRawObject(unwrappedValue, seen)
 }
 
-const toRawObject = (obj: Record<any, any>, seen = new Set()) => Object.keys(obj).reduce((raw, key) => {
-  raw[key] = toRawDeep(obj[key], seen)
-  return raw
-}, {})
+const toRawObject = (obj: Record<any, any>, seen = new Set()) =>
+  Object.keys(obj).reduce((raw, key) => {
+    raw[key] = toRawDeep(obj[key], seen)
+    return raw
+  }, {})
