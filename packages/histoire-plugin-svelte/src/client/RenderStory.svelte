67d775b76f03e23733f49d06ddf73b76d382284b
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-08-11 13:00:10 +0200

    chore: indent

--- packages/histoire-plugin-svelte/src/client/RenderStory.svelte
+++ packages/histoire-plugin-svelte/src/client/RenderStory.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Story } from '@histoire/shared'
+import { Story } from '@histoire/shared'
 import { getContext, setContext } from 'svelte';
 
 const story: Story = getContext('__hstStory')
