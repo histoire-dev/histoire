8230222444c0a66f46c6a7d907e50c71446817dc
ResuBaka <resubaka@gmail.com>
2022-08-11 15:38:07 +0200

    test: add cypress test for markdown links that they are build correctly

---
+++ examples/vue3/src/components/MarkdownLinks.story.vue
@@ -0,0 +1,15 @@
+<template>
+  <Story
+    group="top"
+    docs-only
+    icon="carbon:bookmark"
+  />
+</template>
+
+<docs lang="md">
+[Link to welcome header](#welcome){id="link-to-welcome"}
+
+# Welcome
+
+This is just a link to [Histoire.dev](https://histoire.dev/){id="link-to-history"}.
+</docs>
