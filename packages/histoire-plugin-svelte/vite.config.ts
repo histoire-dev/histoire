f693e7b68fcc7cefb41054a304b78fb99c011179
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-08-12 11:28:41 +0200

    fix: improve build

--- packages/histoire-plugin-svelte/vite.config.ts
+++ packages/histoire-plugin-svelte/vite.config.ts
@@ -1,20 +1,49 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import fs from 'fs-extra'
+import { globbySync } from 'globby'
+import pkg from './package.json'
 
 export default defineConfig({
   plugins: [
     svelte(),
+    {
+      name: 'histoire:preserve:import.dynamic',
+      enforce: 'pre',
+      transform (code) {
+        if (code.includes('import(')) {
+          return {
+            code: code.replace(/import\(/g, 'import__dyn('),
+          }
+        }
+      },
+      closeBundle () {
+        try {
+          const files = globbySync('./dist/**/*.js')
+          for (const file of files) {
+            const content = fs.readFileSync(file, 'utf-8')
+            if (content.includes('import__dyn')) {
+              fs.writeFileSync(file, content.replace(/import__dyn/g, 'import'), 'utf-8')
+            }
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      },
+    },
   ],
   build: {
     emptyOutDir: false,
     outDir: 'dist',
+    cssCodeSplit: false,
     rollupOptions: {
-      external: (source, importer) => {
-        if ((importer?.includes('histoire-plugin-svelte/src') || importer?.startsWith('src/')) && source.startsWith('.')) {
-          return false
-        }
-        return !source.includes('histoire-plugin-svelte/src') && !source.startsWith('src/')
-      },
+      external: [
+        ...Object.keys(pkg.dependencies).map(dep => new RegExp(`^${dep}(\\/?)`)),
+        ...Object.keys(pkg.peerDependencies).map(dep => new RegExp(`^${dep}(\\/?)`)),
+        /^node:/,
+        /^virtual:/,
+        /^\$/, // Virtual modules
+      ],
 
       input: [
         'src/client/index.ts',
