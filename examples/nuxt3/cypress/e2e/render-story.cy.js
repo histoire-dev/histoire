eb3fa585c0f705e53fd647de416213e2e333ffdc
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-08-16 18:36:37 +0200

    test: basic nuxt3 test

---
+++ examples/nuxt3/cypress/e2e/render-story.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Story render', () => {
+  const getIframeBody = () => cy.get('iframe[data-test-id="preview-iframe"]')
+    .its('0.contentDocument.body').should('not.be.empty')
+    .then(cy.wrap)
+
+  it('should display the story content', () => {
+    cy.visit('/story/components-simple-story-vue?variantId=_default')
+    getIframeBody().contains('Simple story in Nuxt NuxtLink')
+  })
+})
