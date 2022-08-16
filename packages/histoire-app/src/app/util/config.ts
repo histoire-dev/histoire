cfd118e3fc730425d5c83de0cae7277ec39f330c
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-08-16 16:22:12 +0200

    chore: fix type import from app

--- packages/histoire-app/src/app/util/config.ts
+++ packages/histoire-app/src/app/util/config.ts
@@ -1,4 +1,4 @@
-import type { HistoireConfig } from '../../../../histoire/src/node/config'
+import type { HistoireConfig } from '@histoire/shared'
 // @ts-expect-error virtual module
 import { config as configRaw, logos as logosRaw } from '$histoire-config'
 
