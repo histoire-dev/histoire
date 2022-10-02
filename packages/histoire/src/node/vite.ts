84f64670ac0aa8bb6a8d64dbcc7e655af4a8474b
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-10-03 01:51:17 +0200

    fix: force optimize vscode-textmate, fix #285

--- packages/histoire/src/node/vite.ts
+++ packages/histoire/src/node/vite.ts
@@ -18,6 +18,7 @@ import { parseColor } from './colors.js'
 import { createMarkdownPlugins } from './markdown.js'
 import { getSearchDataJS, generateDocSearchData, generateTitleSearchData } from './search.js'
 import { getInjectedImport } from './util/vendors.js'
+import { findUp } from './util/find-up.js'
 
 const require = createRequire(import.meta.url)
 
@@ -108,9 +109,14 @@ export async function getViteConfigWithPlugins (isServer: boolean, ctx: Context)
   const inlineConfig = await mergeHistoireViteConfig(userViteConfig?.config ?? {}, ctx)
   const plugins: VitePlugin[] = []
 
+  const hasPnpm = !!(await findUp(ctx.root, ['pnpm-lock.yaml']))
+
   function optimizeDeps (deps: string[]): string[] {
     const result = []
     for (const dep of deps) {
+      if (!hasPnpm) {
+        result.push(dep)
+      }
       try {
         result.push(dirname(require.resolve(`${dep}/package.json`)))
       } catch (e) {
