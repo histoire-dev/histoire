8b21b66ddc063e2c05a54154ced5342a66bd0a5d
Hugo <hugoattal@hotmail.fr>
2022-10-04 14:40:05 +0200

    Fix input text

---
+++ packages/histoire-controls/src/components/button/HstButton.story.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import HstButton from './HstButton.vue'
+</script>
+
+<template>
+  <Story
+    title="HstButton"
+    group="controls"
+    :layout="{ type: 'single', iframe: false }"
+  >
+    <HstButton>
+      Click me!
+    </HstButton>
+  </Story>
+</template>
---
+++ packages/histoire-controls/src/components/button/HstButton.vue
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+export default {
+  name: 'HstButton',
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="htw-text-gray-900 dark:htw-text-gray-100 htw-p-2 htw-rounded-sm active htw-bg-primary-500 hover:htw-bg-primary-600 htw-text-white dark:htw-text-black htw-cursor-pointer">
+    <slot />
+  </button>
+</template>
