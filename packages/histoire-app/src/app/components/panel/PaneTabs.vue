23be901f559ed8db9eb9e1ab98d6fa16d9fb7bfe
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-10-17 16:16:00 +0200

    feat: change default font to Noto Sans Display

--- packages/histoire-app/src/app/components/panel/PaneTabs.vue
+++ packages/histoire-app/src/app/components/panel/PaneTabs.vue
@@ -6,6 +6,7 @@ import type { Story, Variant } from '../../types'
 import BaseOverflowMenu from '../base/BaseOverflowMenu.vue'
 import BaseOverflowTab from '../base/BaseOverflowTab.vue'
 import { useStoryDoc } from './StoryDocs.vue'
+import BaseTag from '../base/BaseTag.vue'
 
 const props = defineProps<{
   story: Story
@@ -45,12 +46,9 @@ const hasEvents = computed(() => eventsStore.events.length)
       }"
     >
       Events
-      <span
-        v-if="eventsStore.unseen"
-        class="htw-text-center htw-text-xs htw-mx-1 htw-px-0.5 htw-h-4 htw-min-w-4 htw-rounded-full active htw-bg-primary-500 htw-text-white dark:htw-text-black"
-      >
-        {{ eventsStore.unseen <=99 ? eventsStore.unseen : "99+" }}
-      </span>
+      <BaseTag v-if="eventsStore.unseen">
+        {{ eventsStore.unseen <= 99 ? eventsStore.unseen : "99+" }}
+      </BaseTag>
     </BaseTab>
 
     <template #overflow>
@@ -77,12 +75,9 @@ const hasEvents = computed(() => eventsStore.events.length)
         }"
       >
         Events
-        <span
-          v-if="eventsStore.unseen"
-          class="htw-text-center htw-text-xs htw-mx-1 htw-px-0.5 htw-h-4 htw-min-w-4 htw-rounded-full active htw-bg-primary-500 htw-text-white dark:htw-text-black"
-        >
+        <BaseTag v-if="eventsStore.unseen">
           {{ eventsStore.unseen <= 99 ? eventsStore.unseen : "99+" }}
-        </span>
+        </BaseTag>
       </BaseOverflowTab>
     </template>
   </BaseOverflowMenu>
