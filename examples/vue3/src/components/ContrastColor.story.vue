9f015a2b6db21a20e5de9d0528b2f0213d9f2417
faga <lzc295592@163.com>
2022-10-19 22:29:09 +0800

    chore: add examples

---
+++ examples/vue3/src/components/ContrastColor.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <Story>
+    <span class="contrast-color">Contrast color</span>
+  </Story>
+</template>
+
+<style scoped>
+.contrast-color{
+    color: var(--contrast-color)
+}
+</style>
