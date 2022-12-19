e389cb07667c88077792e47fbfa2f0b61e5de511
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-12-19 14:20:02 +0100

    feat: upgrade to rollup 3 and vite 4 (#404)

--- packages/histoire-plugin-vue2/vite.config.ts
+++ packages/histoire-plugin-vue2/vite.config.ts
@@ -1,6 +1,35 @@
 import { defineConfig } from 'vite'
+import fs from 'node:fs'
+import { globbySync } from 'globby'
 
 export default defineConfig({
+  plugins: [
+    {
+      name: 'histoire:preserve:import.dynamic',
+      enforce: 'pre',
+      transform (code) {
+        if (code.includes('import(')) {
+          return {
+            code: code.replace(/import\(/g, 'import__dyn('),
+          }
+        }
+      },
+      closeBundle () {
+        try {
+          const files = globbySync('./dist/**/*.js')
+          for (const file of files) {
+            const content = fs.readFileSync(file, 'utf-8')
+            if (content.includes('import__dyn')) {
+              fs.writeFileSync(file, content.replace(/import__dyn\(/g, 'import(/* @vite-ignore */'), 'utf-8')
+            }
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      },
+    },
+  ],
+
   build: {
     emptyOutDir: false,
     outDir: 'dist/bundled',
