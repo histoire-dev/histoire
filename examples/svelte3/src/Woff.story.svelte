171ba08d812a6c738b3472d15447a2cf054325f0
@martin.faeroey <martin.faeroy@gmail.com>
2023-02-21 03:23:51 +0100

    feat(example): story which produces issue

---
+++ examples/svelte3/src/Woff.story.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let Hst
+</script>
+
+<Hst.Story title="Woof" layout={{ type: 'grid' }}>
+  <Hst.Variant title="Dog" source="Dog">
+    🐶
+  </Hst.Variant>
+  <Hst.Variant title="Hot dog" source="Big dog">
+    🐺
+  </Hst.Variant>
+  <Hst.Variant title="Hot dog" source="Hot dog">
+    🌭
+  </Hst.Variant>
+</Hst.Story>
