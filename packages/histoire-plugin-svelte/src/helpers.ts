b99ce84141115a29b702fb519dd9be4073d5bcec
fw6 <45113594+fw6@users.noreply.github.com>
2023-03-17 05:11:11 +0800

    fix(svelte): Supplement missing member types (#483)
    Co-authored-by: Guillaume Chau <guillaume.b.chau@gmail.com>

--- packages/histoire-plugin-svelte/src/helpers.ts
+++ packages/histoire-plugin-svelte/src/helpers.ts
@@ -39,12 +39,14 @@ export interface Hst {
   Number: typeof SvelteComponentTyped<{
     value?: number
     title: string
+    step?: number
   }>
   Slider: typeof SvelteComponentTyped<{
     value?: number
     title: string
     min: number
     max: number
+    step?: number
   }>
   Textarea: typeof SvelteComponentTyped<{
     value?: string
