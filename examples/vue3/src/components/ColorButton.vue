d500dcaea6904cd2b1dd0433b3fd3eb00e4262a6
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-08-04 14:11:22 +0000

    feat(dev): Made a story for color selector control

---
+++ examples/vue3/src/components/ColorButton.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+defineProps({
+    bgcolorselect: {
+        type: String,
+        default: null,
+    },
+    txtcolorselect: {
+        type: String,
+        default: null,
+    },
+})
+</script>
+
+<template>
+    <button :style="{ background: bgcolorselect, color: txtcolorselect }" class="htw-px-2 htw-py-1 htw-rounded-sm btn">
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    cursor: pointer;
+}
+</style>
