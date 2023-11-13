15ef3ea6225237d76fad1a65305c5d18db49723d
Peter Budai <peterbudai@hotmail.com>
2023-11-13 09:27:24 +0100

    types: Fix keyboardShortcut param (#628)

--- packages/histoire-app/src/app/util/tooltip.ts
+++ packages/histoire-app/src/app/util/tooltip.ts
@@ -1,7 +1,7 @@
 import { isMac } from './env.js'
 import { formatKey } from './keyboard.js'
 
-export function makeTooltip (descriptionHtml: string, keyboardShortcut: ({ isMac: boolean }) => string) {
+export function makeTooltip (descriptionHtml: string, keyboardShortcut: (options: { isMac: boolean }) => string) {
   return {
     content: `<div>${descriptionHtml}</div><div class="htw-flex htw-items-center htw-gap-1 htw-mt-2 htw-text-sm">${genKeyboardShortcutHtml(keyboardShortcut({ isMac }))}</div>`,
     html: true,
