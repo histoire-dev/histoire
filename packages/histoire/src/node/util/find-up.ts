ec40fec03e2ff6da1e1bb0cf1aa21eccbf9c2d3b
Mobyrr <59265469+Mobyrr@users.noreply.github.com>
2023-11-25 15:15:47 +0100

    fix: fix findUp for Windows (#640)
    Co-authored-by: Guillaume Chau <guillaume.b.chau@gmail.com>

--- packages/histoire/src/node/util/find-up.ts
+++ packages/histoire/src/node/util/find-up.ts
@@ -2,8 +2,13 @@ import path from 'pathe'
 import fs from 'node:fs'
 
 export function findUp (cwd: string = process.cwd(), fileNames: string[]): string {
-  const { root } = path.parse(cwd)
+  let { root } = path.parse(cwd)
   let dir = cwd
+
+  // On Windows, it will for example return `C:`, we need to add the trailing `/`
+  if (root[1] === ':' && root[2] === undefined) {
+    root += '/'
+  }
 
   while (dir !== root) {
     for (const fileName of fileNames) {
