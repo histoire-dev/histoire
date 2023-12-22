62c9319a6ab830f7c24093c8eb07ed9817a22fcf
Peter Budai <peterbudai@hotmail.com>
2023-12-22 10:16:37 +0100

    fix(app): Fix sandbox URL in getSandboxUrl function
    Fixes #635

--- packages/histoire-app/src/app/util/sandbox.ts
+++ packages/histoire-app/src/app/util/sandbox.ts
@@ -6,5 +6,5 @@ export function getSandboxUrl (story: Story, variant: Variant) {
   const url = new URLSearchParams()
   url.append('storyId', story.id)
   url.append('variantId', variant.id)
-  return `${base}__sandbox.html?${url.toString()}`
+  return `${base}/__sandbox.html?${url.toString()}`
 }
