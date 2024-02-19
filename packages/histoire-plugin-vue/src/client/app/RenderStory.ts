c98417b462a65b68460df7d558e50b9d6939df75
Hugo ATTAL <4563971+hugoattal@users.noreply.github.com>
2024-02-19 03:06:42 +0100

    fix: Add a story root wrapper for Suspense (fix #677) (#678)

--- packages/histoire-plugin-vue/src/client/app/RenderStory.ts
+++ packages/histoire-plugin-vue/src/client/app/RenderStory.ts
@@ -118,8 +118,10 @@ export default _defineComponent({
             )
           }
 
+          // Wrap in div to ensure only one root element
+          children.push(h('div', children.at(-1)))
           // Wrap in Suspense to render async components
-          children.push(h(Suspense, {}, () => children.at(-1)))
+          children.push(h(Suspense, {}, children.at(-1)))
 
           return children.at(-1)
         },
