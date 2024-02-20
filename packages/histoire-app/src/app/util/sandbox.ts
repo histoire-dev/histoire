53b4f8699c786f064d18cc9d8928bb4b76290adf
Morris Swertz <m.a.swertz@rug.nl>
2024-02-20 08:14:10 +0100

    also add empty case

--- packages/histoire-app/src/app/util/sandbox.ts
+++ packages/histoire-app/src/app/util/sandbox.ts
@@ -6,6 +6,6 @@ export function getSandboxUrl (story: Story, variant: Variant) {
   const url = new URLSearchParams()
   url.append('storyId', story.id)
   url.append('variantId', variant.id)
-  const baseUrl = base.endsWith('/') ? base : base + '/'
+  const baseUrl = base.endsWith('/') || base == "" ? base : base + '/'
   return `${baseUrl}__sandbox.html?${url.toString()}`
 }
