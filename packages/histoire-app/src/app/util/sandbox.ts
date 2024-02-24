858be77ab0f67eba2064720544dad20340ae5a87
Morris Swertz <m.a.swertz@rug.nl>
2024-02-24 15:19:55 +0100

    Update sandbox.ts

--- packages/histoire-app/src/app/util/sandbox.ts
+++ packages/histoire-app/src/app/util/sandbox.ts
@@ -6,6 +6,6 @@ export function getSandboxUrl (story: Story, variant: Variant) {
   const url = new URLSearchParams()
   url.append('storyId', story.id)
   url.append('variantId', variant.id)
-  const baseUrl = base == '/' || base == "" ? base : base + '/'
+  const baseUrl = base === '/' || base === "" ? base : base + '/'
   return `${baseUrl}__sandbox.html?${url.toString()}`
 }
