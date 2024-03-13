bffa4878589ce02483a6c6adc91e29fb3bd3eb85
Guillaume Chau <guillaume.b.chau@gmail.com>
2024-03-13 10:01:25 +0100

    Revert "fix: Fix sandbox URL in getSandboxUrl function (#652)"
    This reverts commit 44adb5c2fc5715a6594d13a26b944a28d8cd551b.

--- packages/histoire-app/src/app/util/sandbox.ts
+++ packages/histoire-app/src/app/util/sandbox.ts
@@ -6,6 +6,5 @@ export function getSandboxUrl (story: Story, variant: Variant) {
   const url = new URLSearchParams()
   url.append('storyId', story.id)
   url.append('variantId', variant.id)
-  const baseUrl = base === '/' ? base : base + '/'
-  return `${baseUrl}__sandbox.html?${url.toString()}`
+  return `${base}__sandbox.html?${url.toString()}`
 }
