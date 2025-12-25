dfd29af187f2ec1e6a3a738a7fd5d4f112c974fd
Florent Catiau-Tristant <florent.ct@orange.fr>
2025-12-25 23:26:32 +0100

    feat: create HstDate control component

---
+++ packages/histoire-controls/src/components/date/HstDate.story.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+import HstDate from './HstDate.vue'
+
+function initState() {
+  return {
+    label: 'My really long label',
+    text: '',
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="HstDate"
+    group="controls"
+    :layout="{
+      type: 'grid',
+      width: '100%',
+    }"
+  >
+    <Variant
+      title="default"
+      :init-state="initState"
+    >
+      <template #default="{ state }">
+        <HstDate
+          v-model="state.text"
+          :title="state.label"
+        />
+      </template>
+
+      <template #controls="{ state }">
+        <HstDate
+          v-model="state.label"
+          title="Label"
+        />
+        <HstDate
+          v-model="state.text"
+          title="Text"
+        />
+      </template>
+    </Variant>
+
+    <Variant
+      title="no-label"
+      :init-state="initState"
+    >
+      <template #default="{ state }">
+        <HstDate
+          v-model="state.text"
+          placeholder="Enter some text..."
+        />
+      </template>
+
+      <template #controls="{ state }">
+        <HstDate
+          v-model="state.text"
+          title="Text"
+        />
+      </template>
+    </Variant>
+  </Story>
+</template>
