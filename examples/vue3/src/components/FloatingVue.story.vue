3fe7e9e2a59bfc79cc2f7d8ac822353d667ef7fc
Soryn <soryngitlan@gmail.com>
2026-05-01 20:57:01 +0300

    test: CSS isolation regression across vue3, svelte4, vue3-vuetify

---
+++ examples/vue3/src/components/FloatingVue.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import FloatingVue from './FloatingVue.vue'
+</script>
+
+<template>
+  <Story
+    title="Style Isolation/Floating Vue"
+    :layout="{
+      type: 'grid',
+      width: 360,
+    }"
+  >
+    <Variant title="user-themed">
+      <FloatingVue />
+    </Variant>
+  </Story>
+</template>
